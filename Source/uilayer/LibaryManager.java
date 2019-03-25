21fb1fc24375a85efd21353a6fcfac1ebb02b405
jidibinlin <2694273649@qq.com>
2019-03-25 21:16:45 +0800

    archtect

--- Source/uilayer/LibaryManager.java
+++
@@ -1,5 +0,0 @@
-package uilayer;
-
-public class LibaryManager {
-
-}
---
+++ Source/uilayer/LibraryManager.java
@@ -0,0 +1,10 @@
+package uilayer;
+
+import datalayer.Books;
+
+import java.util.List;
+
+public interface LibraryManager {
+    List<Books> books=null;
+    void prinfBooksInfo();
+}
